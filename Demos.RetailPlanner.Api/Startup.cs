0733b6a95e6005a8ad8af61b9d96bda6bc46b706
KevTurpin <kturpin@gmail.com>
2022-02-22 15:33:12 +0200

    undoing changes, testing configuration on azure

--- Demos.RetailPlanner.Api/Startup.cs
+++ Demos.RetailPlanner.Api/Startup.cs
@@ -25,7 +25,6 @@ namespace Demos.RetailPlanner.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -38,12 +37,11 @@ namespace Demos.RetailPlanner.Api
         {
             //if (env.IsDevelopment())
             //{
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Demos.RetailPlanner.Api v1"));
             //}
 
-            app.UseHttpsRedirection();
             app.UseRouting();
 
             app.UseAuthorization();
