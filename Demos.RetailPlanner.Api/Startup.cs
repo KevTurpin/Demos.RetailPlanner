6981ce3b20aa11fe2db50909b957ca5a6f8ebffa
KevTurpin <kturpin@gmail.com>
2022-02-22 14:49:50 +0200

    diagnose azure issues

--- Demos.RetailPlanner.Api/Startup.cs
+++ Demos.RetailPlanner.Api/Startup.cs
@@ -38,8 +38,8 @@ namespace Demos.RetailPlanner.Api
         {
             //if (env.IsDevelopment())
             //{
-                //app.UseDeveloperExceptionPage();
-                app.UseSwagger();
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Demos.RetailPlanner.Api v1"));
             //}
 
